4bb489bd3d288b7dc54b5d07e51dd58c5dc0622d
Aditya Agrawal <120568867+adityaAg3107@users.noreply.github.com>
2024-05-02 22:12:40 +0530

    Add files via upload

---
+++ Assign_3-API/index.js
@@ -0,0 +1,43 @@
+const postList = document.getElementById('posts');
+
+const fetchPosts = async (url) => {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+        });
+
+        const posts = await response.json();
+
+        return posts;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const displayPosts = async () => {
+    const url = `https://jsonplaceholder.typicode.com/posts`;
+
+    const posts = await fetchPosts(url);
+
+    postList.innerHTML = '';
+
+    posts.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.classList.add('post-card');
+
+        const title = document.createElement('h2');
+        const description = document.createElement('p');
+
+        title.textContent = post.title;
+        description.textContent = post.body;
+
+        postElement.appendChild(title);
+        postElement.appendChild(description);
+
+        postList.appendChild(postElement);
+
+    });
+}
+
+displayPosts();
